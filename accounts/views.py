b130067404240ab26beda6c59a331c75330044be
leeskw <leeskw@nate.com>
2022-05-14 14:54:12 -0700

    django authentication

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,179 @@
-from django.shortcuts import render
+import re
+from django.contrib import messages, auth
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from pkg_resources import resource_string
+from .forms import RegistrationForm
+from .models import Account
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+# Verification email
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
+
+# path('register', views.register, name='register'),
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            phone_number = form.cleaned_data['phone_number']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            username = email.split("@")[0]
+            user = Account.objects.create_user(
+                first_name=first_name, last_name=last_name, email=email, username=username, password=password)
+            user.phone_number = phone_number
+            user.save()
+
+            # Create a user profile
+            # profile = UserProfile()
+            # profile.user_id = user.id
+            # profile.profile_picture = 'default/default-user.png'
+            # profile.save()
+
+            # USER ACTIVATION
+            current_site = get_current_site(request)
+            mail_subject = 'Please activate your account'
+            message = render_to_string('accounts/account_verification_email.html', {
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+            # messages.success(request, 'Thank you for registering with us. We have sent you a verification email to your email address [rathan.kumar@gmail.com]. Please verify it.')
+            return redirect('/accounts/login/?command=verification&email='+email)
+    else:
+        form = RegistrationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/register.html', context)
+
+
+# path('login', views.login, name='login'),
+def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = auth.authenticate(email=email, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in.')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid login credentials')
+            return redirect('login')
+    return render(request, 'accounts/login.html')
+
+
+# path('logout', views.logout, name='logout'),
+@login_required(login_url='login')
+def logout(request):
+    auth.logout(request)
+    messages.success(request, 'You are logged out.')
+    return redirect('login')
+
+
+# path('activate/', views.activate, name='activate'),
+def activate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(
+            request, 'Congratulations! Your account is activated.')
+        return redirect('login')
+    else:
+        messages.error(request, 'Invalid activation link')
+        return redirect('register')
+
+
+# path('dashboard/', views.dashboard, name='dashboard'),
+@login_required
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
+
+
+# path('forgotPassword/', views.forgotPassword, name='forgotPassword'),
+def forgotPassword(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        if Account.objects.filter(email=email).exists():
+            user = Account.objects.get(email__exact=email)
+
+            # Reset password email
+            current_site = get_current_site(request)
+            mail_subject = 'Reset Your Password'
+            message = render_to_string('accounts/reset_password_email.html', {
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+
+            messages.success(
+                request, 'Password reset email has been sent to your email address.')
+            return redirect('login')
+        else:
+            messages.error(request, 'Account does not exist!')
+            return redirect('forgotPassword')
+    return render(request, 'accounts/forgotPassword.html')
+
+
+# path('resetpassword_validate/<uidb64>/<token>/', views.resetpassword_validate, name='resetpassword_validate'),
+def resetpassword_validate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        request.session['uid'] = uid
+        messages.success(request, 'Please reset your password')
+        return redirect('resetPassword')
+    else:
+        messages.error(request, 'This link has been expired!')
+        return redirect('login')
+
+
+# path('resetPassword/', views.resetPassword, name='resetPassword'),
+def resetPassword(request):
+    if request.method == 'POST':
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password == confirm_password:
+            uid = request.session.get('uid')
+            user = Account.objects.get(pk=uid)
+            user.set_password(password)
+            user.save()
+            messages.success(request, 'Password reset successful')
+            return redirect('login')
+        else:
+            messages.error(request, 'Password do not match!')
+            return redirect('resetPassword')
+    else:    
+        return render(request, 'accounts/resetPassword.html')
+    
--- carts/context_processors.py
+++ carts/context_processors.py
@@ -1,3 +1,4 @@
+from accounts.views import activate
 from .models import Cart, CartItem
 from .views import _cart_id
 
@@ -10,7 +11,12 @@ def counter(request):
         try:
             cart = Cart.objects.filter(cart_id=_cart_id(request))
             if request.user.is_authenticated:
-                cart_items = CartItem.objects.all().filter(user=request.user)
+                # cart_items = CartItem.objects.all().filter(user=request.user) # not working
+                """solution: https://stackoverflow.com/questions/42934970/django-error-cannot-resolve-keyword-user-into-field-choices-are
+                Django Error: Cannot resolve keyword 'user' into field. choices are
+                ===================================================================
+                """
+                cart_items = CartItem.objects.all().filter(pk=request.user.pk)
             else:
                 cart_items = CartItem.objects.all().filter(cart=cart[:1])
             for cart_item in cart_items:
