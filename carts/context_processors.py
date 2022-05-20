25b67d92f2e788b71e2bc8b79e784ca84befedc2
leeskw <leeskw@nate.com>
2022-05-20 16:47:01 -0700

    handeled cart and checkout page for logged in users

--- carts/context_processors.py
+++ carts/context_processors.py
@@ -11,12 +11,12 @@ def counter(request):
         try:
             cart = Cart.objects.filter(cart_id=_cart_id(request))
             if request.user.is_authenticated:
-                # cart_items = CartItem.objects.all().filter(user=request.user) # not working
+                cart_items = CartItem.objects.all().filter(user=request.user) # not working
                 """solution: https://stackoverflow.com/questions/42934970/django-error-cannot-resolve-keyword-user-into-field-choices-are
                 Django Error: Cannot resolve keyword 'user' into field. choices are
                 ===================================================================
                 """
-                cart_items = CartItem.objects.all().filter(pk=request.user.pk)
+                # cart_items = CartItem.objects.all().filter(pk=request.user.pk)
             else:
                 cart_items = CartItem.objects.all().filter(cart=cart[:1])
             for cart_item in cart_items:
